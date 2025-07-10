1a206330f49a723ee4559bc0b989e3ef1b70c942
Travis Powell <trpowell@lanl.gov>
2025-07-10 09:15:00 -0600

    Added OHPC Helper script (#20)
    * Pull Specific Testing
    
    Adding a different rpm test build suite for pull requests to be able to
    test and see rpm build issues ahead of tagged builds.
    
    Signed-off-by: Travis Powell <trpowell@lanl.gov>
    
    * RPM Build wasnt included
    
    Somehow. Actually I know how I mis-deleted something, the rpm build
    actually disappeared.
    
    Signed-off-by: Travis Powell <trpowell@lanl.gov>
    
    * Pull Specific Testing
    
    Adding a different rpm test build suite for pull requests to be able to
    test and see rpm build issues ahead of tagged builds.
    
    Signed-off-by: Travis Powell <trpowell@lanl.gov>
    
    * RPM Build wasnt included
    
    Somehow. Actually I know how I mis-deleted something, the rpm build
    actually disappeared.
    
    Signed-off-by: Travis Powell <trpowell@lanl.gov>
    
    * Added OHPC Helper script
    
    Helper script added to RPM for OHPC but can be used for other aspects of
    node file creation?
    
    Signed-off-by: Travis Powell <trpowell@lanl.gov>
    
    * Added BMC MAC back
    
    Signed-off-by: Travis Powell <trpowell@lanl.gov>
    
    * Fixed mis-types bootstrap file
    
    Signed-off-by: Travis Powell <trpowell@lanl.gov>
    
    * Packaged ohpc file
    
    Signed-off-by: Travis Powell <trpowell@lanl.gov>
    
    ---------
    
    Signed-off-by: Travis Powell <trpowell@lanl.gov>

---
+++ scripts/ohpc-nodes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+NAME="$1"
+NID_OFFSET="$2"
+BMC_IP="$3"
+BMC_MAC=$(echo $FQDN|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
+MAC="$4"
+IP_ADDR="$5"
+NID=$((NID_OFFSET + 1))
+XNAME="x1000c0s0b${NID}"
+cat >> /opt/ohpc/admin//nodes/nodes.yaml <<EOT
+- name: ${NAME}
+  nid: ${NID}
+  xname: ${XNAME}
+  bmc_ip: ${BMC_IP}
+  bmc_mac: ${BMC_MAC}
+  group: compute
+  interfaces:
+  - mac_adr: ${MAC}
+    ip_addrs:
+    - name: management
+      ip_addr: ${IP_ADDR}
+EOT
