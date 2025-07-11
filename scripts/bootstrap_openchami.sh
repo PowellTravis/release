2cd236e15c813fd3084a53c70248a6d2e09bc1d5
Travis Powell <trpowell@lanl.gov>
2025-07-11 10:05:10 -0600

    Opaal bootstrap update
    Signed-off-by: Travis Powell <trpowell@lanl.gov>

--- scripts/bootstrap_openchami.sh
+++ scripts/bootstrap_openchami.sh
@@ -46,7 +46,7 @@ acme_correction() {
   sed -i "s/^ContainerName=.*/ContainerName=${system_fqdn}/" /etc/containers/systemd/acme-register.container
   sed -i "s/^HostName=.*/HostName=${system_fqdn}/" /etc/containers/systemd/acme-register.container
   sed -i "s|-d .* \\\\|-d ${system_fqdn} \\\\|" /etc/containers/systemd/acme-register.container
-  sed -i "s|--add-host='demo\.openchami\.cluster:[0-9\.]*'|--add-host='${system_fqdn}:${primary_ip}'|" /etc/openchami/configs/opaal.container
+  sed -i "s|--add-host='demo\.openchami\.cluster:[0-9\.]*'|--add-host='${system_fqdn}:${primary_ip}'|" /etc/containers/systemd/opaal.container
 }
 
 # Check and create secrets with random passwords if needed
